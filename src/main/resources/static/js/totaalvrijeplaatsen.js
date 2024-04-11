74a7dcaf712a3194f5724cf8b938e860ce891083
Adam Hillo <adam1hillo@gmail.com>
2024-04-11 11:50:42 +0200

    static content

---
+++ src/main/resources/static/js/totaalvrijeplaatsen.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import {setText, toon} from "./util.js";
+
+const response = await fetch("films/totaalvrijeplaatsen");
+if (response.ok) {
+    const body = await response.text()
+    setText("totaal", body);
+} else {
+    toon("storing");
+}
