ecf74f83718c5b35c2f3cf96697a5729ee9ce238
Adam Hillo <adam1hillo@gmail.com>
2024-04-11 08:54:29 +0200

    GET

---
+++ src/main/java/be/vdab/cinefest/films/FilmController.java
@@ -0,0 +1,19 @@
+package be.vdab.cinefest.films;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class FilmController {
+
+    private final FilmService filmService;
+
+    FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping("films/totaalvrijeplaatsen")
+    long findTotaalVrijePlaatesen() {
+        return filmService.findTotaalVrijePlaatsen();
+    }
+}
